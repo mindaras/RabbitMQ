9be45e4f428f4112022001c604b6c48c752aba7e
Mindaugas Lazauskas <mind.lazauskas@gmail.com>
2019-01-26 12:42:13 +0200

    added work queues example

---
+++ workQueues/Worker/Worker/Worker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Worker
+{
+    class Worker
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine($"[x] Received {message}");
+
+                    int dots = message.Split(".").Length - 1;
+
+                    Thread.Sleep(dots * 1000);
+
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+
+                    Console.WriteLine("[x] Done");
+                };
+
+                channel.BasicConsume(queue: "task_queue", autoAck: false, consumer: consumer);
+
+                Console.WriteLine("Waiting for messages. To exit press [enter].");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
